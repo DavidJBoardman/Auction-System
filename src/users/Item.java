90d7c38aec5cad84e707b820a967cd9fb9c2759e
Elliot <ellioteb@gmail.com>
2017-03-18 11:55:11 +0000

    this is why we cant have nice things

--- src/users/Item.java
+++ src/users/Item.java
@@ -4,13 +4,10 @@ public class Item {
 	
 	private String name;
 	private String description;
-	private Integer startPrice;
-	private Integer reservePrice;
 	public Item (String name, String description, int startPrice, int reservePrice) {
 		this.name = name;
 		this.description = description;
-		this.startPrice = startPrice;
-		this.reservePrice = reservePrice;
+
 	}
 
 	private String getDescription() {
@@ -29,26 +26,12 @@ public class Item {
 		this.name = name;
 	}
 
-	public Integer getStartPrice() {
-		return startPrice;
-	}
 
-	public void setStartPrice(Integer startPrice) {
-		this.startPrice = startPrice;
-	}
-
-	public Integer getReservePrice() {
-		return reservePrice;
-	}
-
-	public void setReservePrice(Integer reservePrice) {
-		this.reservePrice = reservePrice;
-	}
 
 	//http://stackoverflow.com/questions/13001427/printing-out-all-the-objects-in-array-list
 	@Override
 	public String toString() {
-	    return "Description: " + this.getDescription() + "Name: " + this.getName() + "Start Price: " + this.getStartPrice() + "Reserve Price: " + this.getReservePrice();
+	    return "Description: " + this.getDescription() + "Name: " + this.getName() + "Start Price: ";
 
 	}
 }
