9c927ac9a0d63f31a36a6b4783ef74cf3d07b2fd
Elliot <ellioteb@gmail.com>
2017-03-20 20:53:04 +0000

    added Login Form (WIP)

---
+++ src/auctionSystem/LoginForm.java
@@ -0,0 +1,13 @@
+package auctionSystem;
+
+import javax.swing.*;
+
+/**
+ * Created by Elliot on 20/03/2017.
+ */
+public class LoginForm {
+    private JPanel panel1;
+    private JTextField UsernameField;
+    private JPasswordField passwordField;
+    private JButton logInButton;
+}
