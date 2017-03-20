1bfc09bc20c7546163e217fd863da7a0bb76e9fd
Elliot <ellioteb@gmail.com>
2017-03-20 21:23:45 +0000

    updating UI

--- src/auctionSystem/LoginForm.java
+++ src/auctionSystem/LoginForm.java
@@ -1,6 +1,8 @@
 package auctionSystem;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Elliot on 20/03/2017.
@@ -10,4 +12,15 @@ public class LoginForm {
     private JTextField UsernameField;
     private JPasswordField passwordField;
     private JButton logInButton;
+
+    public LoginForm() {
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String userNameInput = UsernameField.getText();
+                String passwordInput = passwordField.getText();
+                JOptionPane.showMessageDialog(null,userNameInput+"|"+passwordInput);
+            }
+        });
+    }
 }
